1ac046ab93d624e65f2558120df8a7e022d8c8e2
bergson <bergsonacad@gmail.com>
2018-07-12 21:15:43 -0400

    12072018

---
+++ AppLojaVirual/src/main/java/com/Silva/AppLojaVirtual/AppLojaVirualApplication.java
@@ -0,0 +1,12 @@
+package com.Silva.AppLojaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLojaVirualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLojaVirualApplication.class, args);
+	}
+}
